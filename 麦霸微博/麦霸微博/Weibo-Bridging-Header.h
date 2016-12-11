ac219974dc9eec98eccb2b994de0843ea9db1d78
Mybar835 <lijingui2010@163.com>
2016-12-11 18:41:01 +0800

    添加 CZAdditions，通过桥接文件让 Swift 调用 OC 代码
    添加 CZAdditions(这是一个老师写的一些分类)，通过桥接文件让 Swift 调用 OC 代码

---
+++ 麦霸微博/麦霸微博/Weibo-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  麦霸微博-Bridging-Header.h
+//  麦霸微博
+//
+//  Created by lijingui2010 on 2016/12/11.
+//  Copyright © 2016年 MyBar. All rights reserved.
+//
+
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "CZAdditions.h"
