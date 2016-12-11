fcb0e312da702abab48afe6947d2d19c55478738
Mybar835 <lijingui2010@163.com>
2016-12-11 17:42:17 +0800

    设置项目目录结构和启动文件
    设置项目目录结构和启动文件

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Weibo
-练习一下用Swift 3写一个微博项目
+练习一下用 Swift 3.0 写一个微博项目，用的是MVVM模型而不是MVC模型，M表示Model，V表示View(包括View和Controller)，VM表示ViewModel
---
+++ 麦霸微博/麦霸微博/Classes/View(视图和控制器)/Main/MBBaseViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MBBaseViewController.swift
+//  麦霸微博
+//
+//  Created by lijingui2010 on 2016/12/11.
+//  Copyright © 2016年 MyBar. All rights reserved.
+//
+
+import UIKit
+
+class MBBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
